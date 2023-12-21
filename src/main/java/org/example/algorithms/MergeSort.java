2a266413fa50f5d6e825a782f22c81286d761536
Klimenko Vyacheslav <lol.sava.pro@mail.ru>
2023-12-21 16:26:58 +0300

    MergeSort added

--- src/main/java/org/example/algorithms/MergeSort.java
+++ src/main/java/org/example/algorithms/MergeSort.java
@@ -13,7 +13,48 @@ public class MergeSort {
 
     private static void mergSort(int[] arr, int left, int right) {
         if (left < right) {
+            int middle = (left + right) / 2;
 
+            mergSort(arr, left, middle);
+            mergSort(arr, middle + 1, right);
+
+            merg(arr, left, middle, right);
+        }
+    }
+
+    private static void merg(int[] arr, int left, int middle, int right) {
+        int length_left = middle - left + 1;
+        int length_right = right - middle;
+
+        int[] leftArray = new int[length_left];
+        int[] rightArray = new int[length_right];
+
+        System.arraycopy(arr, left, leftArray, 0, length_left);
+        System.arraycopy(arr, middle + 1, rightArray, 0, length_right);
+
+        int i = 0, j = 0, k = left;
+
+        while (i < length_left && j < length_right) {
+            if (leftArray[i] <= rightArray[j]) {
+                arr[k] = leftArray[i];
+                i++;
+            } else {
+                arr[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < length_left) {
+            arr[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        while (j < length_right) {
+            arr[k] = rightArray[j];
+            j++;
+            k++;
         }
     }
 }
